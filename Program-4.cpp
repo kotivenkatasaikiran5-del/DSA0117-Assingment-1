bbf8d4ae3bf00fd1d641d59b3dd04ff12399b4ea
kotivenkatasaikiran5-del <kotivenkatasaikiran5@gmail.com>
2026-03-18 08:29:31 +0530

    Create Program-4.cpp

---
+++ Program-4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float celsius;
+
+    cin >> celsius;
+
+    float fahrenheit = (celsius * 9 / 5) + 32;
+
+    cout << fahrenheit << endl;
+
+    return 0;
+}
